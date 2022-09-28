d9871aeadd4522fb019befffa53f10dc75f0ebe5
edgar <edgarlee135@gmail.com>
2022-09-28 11:06:18 -0700

    user seed

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -5,10 +5,10 @@ from app.models import db, User
 def seed_users():
     demo = User(
         username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='Marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='Bobbie', email='bobbie@aa.io', password='password')
+    keshi = User(
+        username='keshi', email='keshi@aa.io', password='password')
+    dpr = User(
+        username='DPR Live', email='dprl@aa.io', password='password')
     jay = User(
         username='Jay Park', email='jaypk@aa.io', password='password')
     jack = User(
@@ -26,8 +26,8 @@ def seed_users():
     lisa = User(
         username='Lisa', email='lisa@aa.io', password='password')
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(keshi)
+    db.session.add(dpr)
     db.session.add(jay)
     db.session.add(jack)
     db.session.add(jimin)
